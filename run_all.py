9de6cad37232707301e5b08fa0eac7fcf901e128
HellR@y <47401874+DanjokLion@users.noreply.github.com>
2023-11-20 16:46:09 +0300

    add freelance.ru, freeru.py - dont work

--- run_all.py
+++ run_all.py
@@ -1,6 +1,6 @@
 import subprocess
 
-files = ['habr.py']
+files = ['habr.py', 'freeru.py']
 
 for file in files:
     subprocess.Popen(args=['start', 'python', file], shell=True, stdout=subprocess.PIPE)
