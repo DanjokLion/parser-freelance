dabbac5250f50bf090919fc3752a22750e74d111
HellR@y <47401874+DanjokLion@users.noreply.github.com>
2023-11-20 18:07:15 +0300

    corrert request all orders

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ app = FastAPI()
 with open(f'habr_{datetime.datetime.now().strftime("%d_%m_%Y")}.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-@app.post('/habr')
-async def get_habr_item(item: HabrItem):
-    res = {
-        'name': item.name,
-        'price': item.price,
-        'link': item.link
-    }
-    return res
+@app.get('/habr')
+async def get_habr_item():
+    return data
