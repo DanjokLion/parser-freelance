df765ab6c3c22062cec7e23c58eaa63d8be385e3
HellR@y <47401874+DanjokLion@users.noreply.github.com>
2023-11-24 12:59:14 +0300

    get request freelancejob

--- server/app.py
+++ server/app.py
@@ -5,8 +5,16 @@ import json, datetime
 app = FastAPI()
 
 with open(f'habr_{datetime.datetime.now().strftime("%d_%m_%Y")}.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
+    habr_data = json.load(file)
+
+with open(f'freejob_{datetime.datetime.now().strftime("%d_%m_%Y")}.json', 'r', encoding='utf-8') as file:
+    freej_data = json.load(file)
+
 
 @app.get('/habr')
 async def get_habr_item():
-    return data
+    return habr_data
+
+@app.get('/freelancejob')
+async def get_freejob_item():
+    return freej_data
